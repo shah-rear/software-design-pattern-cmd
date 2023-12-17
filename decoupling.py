81bfa59bf4a4255cb9c1a631ac73db192b733287
shajib1927 <shajibshahrear@gmail.com>
2023-12-17 21:24:21 +0300

    first commit

---
+++ decoupling.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+class Command(ABC):
+    @abstractmethod
+    def execute(self):
+        pass
+
+class ConcreteCommandA(Command):
+    def __init__(self, receiver):
+        self.receiver = receiver
+
+    def execute(self):
+        self.receiver.perform_operation_A()
+
+class Receiver:
+    def perform_operation_A(self):
+        print("Receiver is performing Operation A")
+
+class Invoker:
+    def __init__(self):
+        self.command = None
+
+    def set_command(self, command):
+        self.command = command
+
+    def execute_command(self):
+        self.command.execute()
+
+# Client code
+receiver, concrete_command, invoker = Receiver(), ConcreteCommandA(Receiver()), Invoker()
+invoker.set_command(concrete_command)
+invoker.execute_command()
---
+++ open_close.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+
+class Command(ABC):
+    @abstractmethod
+    def execute(self):
+        pass
+
+class ConcreteCommandA(Command):
+    def execute(self):
+        print("Executing Command A")
+
+class ConcreteCommandB(Command):
+    def execute(self):
+        print("Executing Command B")
+
+class Receiver:
+    def perform_operation_A(self):
+        print("Receiver is performing Operation A")
+
+class Invoker:
+    def __init__(self, commands=[]):
+        self.commands = commands
+
+    def add_command(self, command):
+        self.commands.append(command)
+
+    def execute_commands(self):
+        [command.execute() for command in self.commands]
+
+# Client code
+receiver = Receiver()
+invoker = Invoker([ConcreteCommandA(), ConcreteCommandB()])
+
+invoker.execute_commands()
